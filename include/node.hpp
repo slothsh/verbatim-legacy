f4215cc949c0107cc153ba846d413aad22048bf5
Stefan Olivier <snowflake1194@gmail.com>
2021-07-24 20:52:02 +0200

    added enumerable_xx concepts for ns, tag, attr, attropt, vexpr, content

--- include/node.hpp
+++ include/node.hpp
@@ -5,6 +5,10 @@
 #ifndef VTNODE_HEADER
 #define VTNODE_HEADER
 
+// Redefinitions for magic enum size constants
+#define MAGIC_ENUM_RANGE_MIN 0
+#define MAGIC_ENUM_RANGE_MAX 256
+
 // Standard headers
 #include <iostream>
 #include <limits>
@@ -22,41 +26,51 @@ namespace vt
 {
 	namespace dictionary
 	{
+        // Dictionary constants
+        static constexpr size_t ENUM_ID = MAGIC_ENUM_RANGE_MIN;
+        static constexpr size_t ENUM_NONE = MAGIC_ENUM_RANGE_MAX;
+
 		// Enumerations
 		enum class NS : size_t
 		{
+            IS_NS = ENUM_ID,
 			one, two, three, four,
-			none = std::numeric_limits<size_t>::max()
+			none = ENUM_NONE
 		};
 
 		enum class Tag : size_t
 		{
+            IS_TAG = ENUM_ID,
 			one, two, three, four,
-			none = std::numeric_limits<size_t>::max()
+			none = ENUM_NONE
 		};
 
 		enum class Attribute : size_t
 		{
+            IS_ATTRIBUTE = ENUM_ID,
 			one, two, three, four,
-			none = std::numeric_limits<size_t>::max()
+			none = ENUM_NONE
 		};
 
 		enum class ValueExpression : size_t
 		{
+            IS_VALUE_EXPRESSION = ENUM_ID,
 			one, two, three, four,
-			none = std::numeric_limits<size_t>::max()
+			none = ENUM_NONE
 		};
 
 		enum class AttributeOption : size_t
 		{
+            IS_ATTRIBUTE_OPTION = ENUM_ID,
 			one, two, three, four,
-			none = std::numeric_limits<size_t>::max()
+			none = ENUM_NONE
 		};
 
 		enum class Content : size_t
 		{
+            IS_CONTENT = ENUM_ID,
 			one, two, three, four,
-			none = std::numeric_limits<size_t>::max()
+			none = ENUM_NONE
 		};
 	}
 }
@@ -70,10 +84,57 @@ namespace vt
 {
 	namespace dictionary
 	{
+        using namespace magic_enum;
+
+        // Concept for generic enumeration types
 		template<class E>
 		concept enumerable_node =
 			std::is_enum_v<E>
-			&& (E::none == E::none);
+            && is_scoped_enum_v<E>
+            && enum_contains<E>(E::none)
+			&& enum_integer(E::none) == ENUM_NONE;
+
+        // Concept for enumeration types to be used for namespaces
+        template<class Tns>
+        concept enumerable_ns =
+            enumerable_node<Tns>
+            && enum_contains<Tns>(Tns::IS_NS)
+            && enum_integer(Tns::IS_NS) == ENUM_ID;
+
+        // Concept for enumeration types to be used for xml tags
+        template<class Ttag>
+        concept enumerable_tag =
+            enumerable_node<Ttag>
+            && enum_contains<Ttag>(Ttag::IS_TAG)
+            && enum_integer(Ttag::IS_TAG) == ENUM_ID;
+
+        // Concept for enumeration types to be used for attributes
+        template<class Tattr>
+        concept enumerable_attr =
+            enumerable_node<Tattr>
+            && enum_contains<Tattr>(Tattr::IS_ATTRIBUTE)
+            && enum_integer(Tattr::IS_ATTRIBUTE) == ENUM_ID;
+
+        // Concept for enumeration types to be used for attributes
+        template<class Tvexpr>
+        concept enumerable_vexpr =
+            enumerable_node<Tvexpr>
+            && enum_contains<Tvexpr>(Tvexpr::IS_VALUE_EXPRESSION)
+            && enum_integer(Tvexpr::IS_VALUE_EXPRESSION) == ENUM_ID;
+
+        // Concept for enumeration types to be used for attributes
+        template<class Topt>
+        concept enumerable_attropt =
+            enumerable_node<Topt>
+            && enum_contains<Topt>(Topt::IS_ATTRIBUTE_OPTION)
+            && enum_integer(Topt::IS_ATTRIBUTE_OPTION) == ENUM_ID;
+
+        // Concept for enumeration types to be used for attributes
+        template<class Tcont>
+        concept enumerable_content =
+            enumerable_node<Tcont>
+            && enum_contains<Tcont>(Tcont::IS_CONTENT)
+            && enum_integer(Tcont::IS_CONTENT) == ENUM_ID;
 
 		// Generic types
 		template<enumerable_node E>
@@ -128,14 +189,30 @@ namespace vt
 			size_t                      conditions;
 		};
 
-		template<class Tns, class Ttag, class Tattr, class Tvexpr, class Topt, class Tdata,
+        // General XML node type for XML dictionary entries
+		template<enumerable_ns Tns, enumerable_tag Ttag,
+                    enumerable_attr Tattr, enumerable_vexpr Tvexpr, enumerable_attropt Topt,
+                    enumerable_content Tdata,
 					size_t SizeAttr, size_t SizeVexpr, size_t SizeOpt, size_t SizeData>
 		struct XMLNode
 		{
-			Node<Tns, Ttag>                                                  element;
-			AttributeNode<Tns, Tattr, Tvexpr, Topt, SizeVexpr, SizeOpt>      attributes[SizeAttr];
-			ContentNode<Tns, Tdata>                                          content[SizeData];
-			size_t                                                           documents;
+			using node_t = Node<Tns, Ttag>;
+			using attribute_t = AttributeNode<Tns, Tattr, Tvexpr, Topt, SizeVexpr, SizeOpt>;
+			using content_t = ContentNode<Tns, Tdata>;
+
+            // template<enumerable_node Ens>
+            // using enum_t = Ens;
+
+            // // Constructor
+            // constexpr XMLNode(Tns&& ns) noexcept
+            // {
+                
+            // }
+
+			node_t				element;	
+			attribute_t			attributes[SizeAttr];
+			content_t			content[SizeData];
+			size_t				documents;
 		};
 
 // XML node dictionary template ----------------------------------------------------------------------------------------3 of 3-|
--- src/prototype/ttmldictionary.cpp
+++ src/prototype/ttmldictionary.cpp
@@ -11,25 +11,26 @@
 int main()
 {
 	using namespace vt::dictionary;
-	
+
     constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
             1, 1, 1, 1> a{};
-    constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
-            2, 2, 2, 2> b{};
-    constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
-            3, 3, 3, 3> c{};
-    constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
-            4, 4, 4, 4> d{};
-    constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
-            5, 5, 5, 5> e{};
+    // constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
+    //         2, 2, 2, 2> b{};
+    // constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
+    //         3, 3, 3, 3> c{};
+    // constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
+    //         4, 4, 4, 4> d{};
+    // constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
+    //         5, 5, 5, 5> e{};
 
     // constexpr XMLDictionary<decltype(a), decltype(b), decltype(c)> dict{};
     // const XMLDictionary<decltype(a), decltype(b), decltype(c), decltype(d)> dict(a,b,c,d);
-    constexpr XMLDictionary<decltype(a), decltype(b), decltype(c), decltype(d)> dict(a,b,c,d);
-    dict.get<1>();
-    dict.get<2>();
-    dict.get<3>();
+    // constexpr XMLDictionary<decltype(a), decltype(b), decltype(c), decltype(d)> dict(a,b,c,d);
+    // dict.get<1>();
+    // dict.get<2>();
+    // dict.get<3>();
 
-    std::cout << "end\n";
+	// std::cout << std::boolalpha << magic_enum::enum_contains<NS>(NS::none) << '\n';
+    std::cout << "\nend\n";
     return 0;
 }
