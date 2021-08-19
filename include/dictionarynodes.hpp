fe80b5e040279152ff86bc1e66e8a8173e3d86de
SoulXP <snowflake1194@gmail.com>
2021-08-19 19:10:45 +0200

    removed old CreateTTMLNode() helper function from dictionarynodes.hpp; cleaned up old test code in ttmldictionary.cpp

--- include/dictionarynodes.hpp
+++ include/dictionarynodes.hpp
@@ -387,62 +387,6 @@ namespace vt::dictionary
     };
 }
 
-// XML node dictionary template ----------------------------------------------------------------------------------------4 of 5-|
-// ============================================================================================================================|
-
-namespace vt::dictionary::detail
-{
-    template<class Tns = NS, class Ttag = Tag,
-                class Tattr_elem, class Tattr_vexpr, class Tattr_flags,
-                class Tfnattr, class Tfndata> // TODO: invocable concept for Tfn
-    constexpr inline auto CreateTTMLNode(const size_t&& docs, const Tns&& ns, const Ttag&& tag,
-                                            const Tattr_elem&& init_attr_element, const Tattr_vexpr&& init_attr_vexpr, const Tattr_flags&& init_attr_flags,
-                                            const Tfnattr&& fn_attr, const Tfndata&& fn_data)
-    {
-        // static_assert(Tns != NS::none && Ttag != Tag::none, "Invalid TTMLNode type\n");
-
-        using ns_t = decltype(ns);
-        using tag_t = decltype(tag);
-        using attr_element_t = decltype(init_attr_element);
-        using attr_vexpr_t = decltype(init_attr_vexpr);
-        using attr_flags_t = decltype(init_attr_flags);
-
-        const auto attributes = fn_attr (
-            std::forward<attr_element_t>(init_attr_element),
-            std::forward<attr_vexpr_t>(init_attr_vexpr),
-            std::forward<attr_flags_t>(init_attr_flags)
-        );
-
-        // const auto content = fn_data(std::move(ns), std::move(tag));
-        constexpr ContentNode content{ 0, 0, NS::none, Tag::none };
-
-        using node_t = DictionaryNode<NS, Tag, decltype(attributes), decltype(content)>;
-
-        return node_t {
-            std::move(docs),
-            std::forward<ns_t>(ns), std::forward<tag_t>(tag),
-            std::move(attributes), std::move(content)
-        };
-    }
-
-    template<class Ttup, class S, S... Sseq>
-    constexpr inline auto CreateTTMLNodeList(const Ttup&& tup, std::integer_sequence<S, Sseq...> sequence)
-    {
-        return std::tuple {
-            CreateTTMLNode(
-                std::move(std::get<0>(std::get<Sseq>(tup))),
-                std::move(std::get<1>(std::get<Sseq>(tup))),
-                std::move(std::get<2>(std::get<Sseq>(tup))),
-                std::move(std::get<3>(std::get<Sseq>(tup))),
-                std::move(std::get<4>(std::get<Sseq>(tup))),
-                std::move(std::get<5>(std::get<Sseq>(tup))),
-                std::move(std::get<6>(std::get<Sseq>(tup))),
-                std::move(std::get<7>(std::get<Sseq>(tup)))
-            )...
-        };
-    }
-}
-
 // ------------------------------------------------------------|END|-----------------------------------------------------------|
 
 #endif // VTNODE_HEADER
--- src/prototype/ttmldictionary.cpp
+++ src/prototype/ttmldictionary.cpp
@@ -19,35 +19,6 @@ int main()
 {
 	using namespace vt::dictionary;
 
-    // SECTION 1
-    // constexpr XMLNode<NS, Tag, NS, Attribute, ValueExpression, AttributeOption, NS, Content,
-    //         1, 1, 1, 1> a{};
-    // constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
-    //         2, 2, 2, 2> b{};
-    // constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
-    //         3, 3, 3, 3> c{};
-    // constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
-    //         4, 4, 4, 4> d{};
-    // constexpr XMLNode<NS, Tag, Attribute, ValueExpression, AttributeOption, Content,
-    //         5, 5, 5, 5> e{};
-
-    // constexpr XMLDictionary<decltype(a), decltype(b), decltype(c)> dict{};
-    // const XMLDictionary<decltype(a), decltype(b), decltype(c), decltype(d)> dict(a,b,c,d);
-    // constexpr XMLDictionary<decltype(a), decltype(b), decltype(c), decltype(d)> dict(a,b,c,d);
-    // dict.get<1>();
-    // dict.get<2>();
-    // dict.get<3>();
-
-	// std::cout << std::boolalpha << magic_enum::enum_contains<NS>(NS::none) << '\n';
-
-    // SECTION 2
-    // vt::dictionary::detail::EntryCollector<size_t, size_t, size_t, size_t> a{1,2,3,4};
-
-    // auto r = a.GetSize();
-    // std::cout << "Accumulate total: " << vt::dictionary::detail::accumulate_t::total << '\n';
-    // std::cout << "Total entries: " << r << '\n';
-
-    // SECTION 3
     static_assert(MAGIC_ENUM_RANGE_MIN == 0);
     static_assert(MAGIC_ENUM_RANGE_MAX == 256);
 
